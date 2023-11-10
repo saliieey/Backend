72f0bfa114e0d81cd8975dab423400a3c5978b94
saliieey <salihck7777@gmail.com>
2023-11-10 08:47:34 +0530

    json

---
+++ index.js
@@ -0,0 +1,33 @@
+    //import json-server library in index.js file
+
+   const jsonServer= require ('json-server')
+
+
+
+   //create json-server
+
+   const mediaPlayerServer = jsonServer.create()
+
+  //Create path json path
+  const router = jsonServer.router('db.json')
+
+
+
+   //middleware to convert json to js
+
+   const middleware =jsonServer.defaults()
+
+   //use middleware and router in server
+
+   mediaPlayerServer.use(middleware)
+   mediaPlayerServer.use(router)
+
+   //setup port for running server
+
+   const port =5000 ||process.env.PORT
+
+   //to listen server for resolving request from client
+
+   mediaPlayerServer.listen(port,()=>{
+    console.log(`mediaplayerserver started at ${port} and waiting for the request`);
+   })
